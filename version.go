c45b4b900482664c2f831b34deb1fdf745406f5f
oliwoli <oliverweiss97@gmail.com>
2025-07-22 18:29:16 +0200

    feat: Embed package.json for versioning in production
    Embed the frontend/package.json file into the Go binary using
    to ensure the application version is available in production builds.
    The  variable now reads from the embedded file instead of
    the filesystem.

--- version.go
+++ version.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"embed"
 	"encoding/json"
 	"log"
-	"os"
 )
+
+//go:embed package.json
+var content embed.FS
 
 type PackageJSON struct {
 	Version string `json:"version"`
@@ -13,13 +16,13 @@ type PackageJSON struct {
 var AppVersion string
 
 func init() {
-	content, err := os.ReadFile("package.json")
+	file, err := content.ReadFile("package.json")
 	if err != nil {
-		log.Fatalf("Error reading package.json: %v", err)
+		log.Fatalf("Error reading embedded package.json: %v", err)
 	}
 
 	var pkg PackageJSON
-	err = json.Unmarshal(content, &pkg)
+	err = json.Unmarshal(file, &pkg)
 	if err != nil {
 		log.Fatalf("Error unmarshalling package.json: %v", err)
 	}
